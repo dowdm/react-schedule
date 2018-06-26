38d1b6a88a8617b53bff4c8016125b215780ec4c
Matt Dowdney and William Chu <william@>
2018-06-26 10:05:09 -0700

    add loop to display events

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Farm from "./Farm"
+
+function App(){
+  return (
+    <div>
+      <Farm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Farm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Farm(){
+  return (
+    <div>
+      <style global jsx>{`
+          div {
+            background-color: red;
+          }
+        `}</style>
+      <h3>FARM</h3>
+    </div>
+  );
+}
+
+
+export default Farm;
